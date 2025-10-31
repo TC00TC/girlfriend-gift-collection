ea36b775dd7a1b67e102dfafc030077c0f9a7aa9
TC00TC <tiance.king@outlook.com>
2025-10-31 10:21:17 +0800

    Update birthday messages and associated images变更js
    Update birthday messages and associated images变更js

--- birth-want-to-say/config.js
+++ birth-want-to-say/config.js
@@ -4,20 +4,20 @@ var config = {
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
         "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的姑娘",  // 同上...
+        "最心爱的孙蔚蔚❥(^_-)",  // 同上...
         "今天是你的生日",
         "这是我们在一起的",
-        "第三个生日！",
-        "这两年来，我见过你",
-        "奇怪的样子",
-        "可爱的样子",
-        "生气的样子",
-        "都是我喜欢的样子",
-        "对了，礼物收全了吗？",
-        "今年要吃好好的",
-        "喂饱饱的",
-        "然后才有力气干活",
-        "和想我！",
+        "第一个生日！",
+        "这10个月以来，我们去过很多地方",
+        "沈阳周边",
+        "珠海",
+        "江苏",
+        "还有很多地方~",
+        "今年的礼物也许有点特别~",
+        "我希望你能喜欢",
+        "等我回去",
+        "然后还有其他礼物~",
+        "以及爱你的我！",
         "生日快乐~~",
     ],
     /**
@@ -31,21 +31,21 @@ var config = {
      */
     imgs: {
         "送给我": "./imgs/songxinxin.jpeg",
-        "心爱的姑娘": "./imgs/nothing.jpg",
+        "最心爱的孙蔚蔚❥(^_-)": "./imgs/qipao.jpg",
         "今天是你的生日": "./imgs/shengri.jpeg",
-        "这是我们在一起的": "./imgs/zaiyiqi.png",
-        "第三个生日！": "./imgs/nothing.jpg",
-        "这两年来，我见过你": "./imgs/yangzi.jpeg",
-        "奇怪的样子": "./imgs/nothing.jpg",
-        "可爱的样子": "./imgs/nothing.jpg",
-        "生气的样子": "./imgs/nothing.jpg",
-        "都是我喜欢的样子": "./imgs/nothing.jpg",
-        "对了，礼物收全了吗？": "./imgs/chuo.gif",
-        "今年要吃好好的": "./imgs/chihaodian.jpeg",
-        "喂饱饱的": "./imgs/weibao.jpeg",
-        "然后才有力气干活": "./imgs/nothing.jpg",
-        "和想我！": "./imgs/nothing.jpg",
-        "生日快乐~~": "./imgs/nothing.jpg"
+        "这是我们在一起的": "./imgs/chi.jpg",
+        "第一个生日！": "./imgs/kaka.jpg",
+        "这10个月以来，我们去过很多地方": "./imgs/yiqi.jpeg",
+        "沈阳周边": "./imgs/liaoning1.jpg",
+        "珠海": "./imgs/zhuhai.jpg",
+        "江苏": "./imgs/suzhou.jpg",
+        "还有很多地方": "./imgs/xian2.jpg",
+        //"今年的礼物也许有点特别~": "./imgs/chuo.gif",
+        "我希望你能喜欢": "./imgs/xian1.jpeg",
+        "等我回去": "./imgs/yu.jpg",
+        "然后还有其他礼物~": "./imgs/zhuhai2.jpg",
+        "以及爱你的我！": "./imgs/biaobai.jpg",
+        "生日快乐~~": "./imgs/biaobai.jpg"
     },
     
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
@@ -61,5 +61,5 @@ var config = {
     },
 
     // 结束语
-    loveText: '爱你的X先生~'
+    loveText: '爱你的田先生~'
 };
